59cec8a7f37c71d14a69cf938998b563336fb937
KaKoKa5 <bonfardeci.saverio@e-artsup.net>
2026-09-14 18:35:18 +0200

    FirstCardEver créé

---
+++ Assets/Project/Scripts/Gameplay/Data/Card/CardInfoData.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace GamePlay
+{
+    [CreateAssetMenu(fileName = "CardInfoData", menuName = "CardInfoData")]
+    public class CardInfoData : ScriptableObject
+    {
+	    [Header("Informations Générales")]
+	    [field:SerializeField] public string cardName;
+	    [field:SerializeField] private Sprite cardIcon;
+	    [field:SerializeField] private GameObject cardPrefab;
+
+	    [Header("Attributs de Jeu")]
+	    [field:SerializeField] private CardStyle style;
+	    [field:SerializeField] private CardRarity rarity;
+	    [field:SerializeField] private CardType type;
+	    [field:SerializeField, Range(1,8)] private int baseValue;  
+    }
+}
---
+++ Assets/Project/Scripts/Gameplay/Data/Card/Enum.cs
@@ -0,0 +1,26 @@
+namespace GamePlay
+{
+    public enum CardStyle
+    {
+	    Boxe,
+	    MuayThai,
+	    Taekwondo,
+	    KungFu,
+	    Street,
+	    Catch,
+	    Sambo
+    }
+
+    public enum CardType
+    {
+	    Attacker,
+	    Defender,
+    }
+    public enum CardRarity
+    {
+	    Common,
+	    Uncommon,
+	    Rare,
+	    Legendary,
+    }
+}
